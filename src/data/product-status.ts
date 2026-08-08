9d824794f1062945ef37307609a94baab0155182
Yaniv Raz <yanivmiz77@gmail.com>
2026-08-09 02:20:15 +0300

    chore: remove unnecessary status helper

--- src/data/product-status.ts
+++
@@ -1,8 +0,0 @@
-import { canonicalGrades } from './canonical-content';
-import { middleSchoolResourceCount } from './site-counts';
-
-export const productStatus = {
-  canonicalGradeCount: canonicalGrades.length,
-  middleSchoolResourceCount,
-  hasThreeMiddleSchoolGrades: canonicalGrades.map((grade) => grade.slug).join(',') === 'z,h,t',
-} as const;
